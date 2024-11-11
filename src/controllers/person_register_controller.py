70313cc5d08a9b58322044f0888c2e40de33219e
Mateus Prado <mateusp2000@hotmail.com>
2024-11-11 20:43:04 -0300

    feat: Implementing model dir

--- src/controllers/person_register_controller.py
+++ src/controllers/person_register_controller.py
@@ -1,4 +1,9 @@
+from src.models.db.repositories.people_repository import PeopleRepository
+
 class PersonRegisterController:
+    def __init__(self) -> None:
+        self.__person_repository = PeopleRepository()
+
     def create_person(self, person_name: str, person_age: int) -> dict:
         self.__validate_person_registry(person_name)
         self.__insert_person(person_name, person_age)
@@ -6,11 +11,11 @@ class PersonRegisterController:
         return response
     
     def __validate_person_registry(self, person_name: str) -> None:
-        person = None
+        person = self.__person_repository.get_person_by_name(person_name)
         if person: raise Exception("Person already registered")
 
     def __insert_person(self, person_name: str, person_age: int) -> None:
-        pass
+        self.__person_repository.registry_person(person_name, person_age)
 
     def __format_response(self, person_name) -> dict:
         return {
